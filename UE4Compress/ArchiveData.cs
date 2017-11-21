45d0be6f35f49208040a9e90373b03607c4a15a4
Lorien <jonathan@bourassa.family>
2017-11-21 14:39:23 -0500

    Header prasing concept.

---
+++ UE4Compress/ArchiveData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UE4Compress
+{
+    public class ArchiveData
+    {
+        public FileHeader FileHeader { get; set; }
+
+        public List<byte[]> Chunks { get; set; }
+
+        public ArchiveData(byte[] archiveData)
+        {
+            FileHeader = new FileHeader(archiveData);
+            Chunks = new List<byte[]>();
+            var chunkStartIndex = (long)FileHeader.FileHeaderSize + FileHeader.ChunkHeaderSize * FileHeader.ChunkHeaders.Count;
+            foreach (var chunkHeader in FileHeader.ChunkHeaders)
+            {
+                var curChunkData = new byte[chunkHeader.CompressedBuffer];
+                Array.Copy(archiveData, chunkStartIndex, curChunkData, 0, chunkHeader.CompressedBuffer);
+                Chunks.Add(curChunkData);
+                chunkStartIndex += chunkHeader.CompressedBuffer;
+            }
+        }
+    }
+}
---
+++ UE4Compress/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UE4Compress
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var testFile = File.ReadAllBytes(".\\RedWood_Tree_2_D_Test.uasset.z");
+            var archive = new ArchiveData(testFile);
+            Console.WriteLine($"This Archive has {archive.Chunks.Count} chunks.");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
